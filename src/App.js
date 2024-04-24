dcae9d593faff151b146734092433bd413075618
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-04-24 10:28:53 +0530

    Final commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ExpensePage from './pages/ExpensePage/ExpensePage';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <ExpensePage></ExpensePage>
+      </div>
+    </div>
+  );
+}
+
+export default App;
